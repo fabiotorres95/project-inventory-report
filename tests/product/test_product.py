2943ded36d6fe5e3e1d3e436ef649fe172d929c7
fabiotorres95 <engfabiotorres@outlook.com>
2023-10-31 14:41:51 -0300

    test_create_product done

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,2 +1,21 @@
+from inventory_report.product import Product
+
+
 def test_create_product() -> None:
-    raise NotImplementedError
+    good_product = Product(
+        "123",
+        "product_name",
+        "company_name",
+        "manufacturing_date",
+        "expiration_date",
+        "serial_number",
+        "storage_instructions",
+    )
+
+    assert good_product.id == "123"
+    assert good_product.product_name == "product_name"
+    assert good_product.company_name == "company_name"
+    assert good_product.manufacturing_date == "manufacturing_date"
+    assert good_product.expiration_date == "expiration_date"
+    assert good_product.serial_number == "serial_number"
+    assert good_product.storage_instructions == "storage_instructions"
