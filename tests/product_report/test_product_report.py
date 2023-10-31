9e3a88c211841cb1eec53a18bd6b31c979ce50c5
fabiotorres95 <engfabiotorres@outlook.com>
2023-10-31 15:01:32 -0300

    test_product_report done

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,2 +1,23 @@
+from inventory_report.product import Product
+
+
 def test_product_report() -> None:
-    raise NotImplementedError
+    good_report = Product(
+        "1",
+        "Banana",
+        "Banana Republic",
+        "01-01-2021",
+        "02-02-2022",
+        "987654321",
+        "Eat it before it rots",
+    )
+
+    assert str(good_report) == (
+        "The product 1 - Banana "
+        "with serial number 987654321 "
+        "manufactured on 01-01-2021 "
+        "by the company Banana Republic "
+        "valid until 02-02-2022 "
+        "must be stored according to the following instructions: "
+        "Eat it before it rots."
+    )
